db421e83096040e1d814d042eb7c37f8ea7f1eaa
Justin LeCheminant <jlechem@gmail.com>
2020-09-03 20:24:08 -0600

    Initial Commit

---
+++ CryptoExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using CryptoExamples.Utilities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CryptoExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var foo = KeyGenerator.GenerateKey();
+                Console.WriteLine(foo);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CryptoExamples/Utilities/KeyGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;   
+using System.Text;
+
+namespace CryptoExamples.Utilities
+{
+    public static class KeyGenerator
+    {
+        private static Random _random = new Random();
+
+        public static string GenerateKey(int length = 1028)
+        {
+            if (length < 1028)
+            {
+                throw new ArgumentException($"Invalid key length {length}");
+            }
+
+            var result = String.Empty;
+
+            for (var i = 0; i < length; i++)
+            {
+                var letter = (char)_random.Next(32, 126);
+                result = $"{result}{letter}";
+            }
+
+            return result;
+        }
+
+    }
+}
